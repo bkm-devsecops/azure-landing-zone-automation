c6261adce738da763b9140815a681fcb18358126
Brajendra Mishra <mishrabrajendrabcrec@gmail.com>
2026-06-23 13:39:30 +0530

    changes done in root module

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ module "compute" {
   source = "./modules/Compute"
   nic    = var.nic # 👈 CHECK 1: Left side par 'nics' hona chahiye kyuki child variables.tf mein 'nics' declare hai
   vms    = var.vms
+  subnet_id           = module.networking.subnet_ids
   #   subnet_mapping = module.networking.subnet_ids # 👈 CHECK 2: 'subnet_ids' (plural) aur module name lowercase mein match ho gaya
   depends_on = [module.networking]
 }
